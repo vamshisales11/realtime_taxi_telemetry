ad59932c0c97cba7fb534f4ad03e544c60c052f6
vamshiawsde <vamshisales11@gmail.com>
2025-11-15 14:14:10 -0500

    kinesis data stream deployed

--- realtime_taxi_telemetry/realtime_taxi_telemetry_stack.py
+++ realtime_taxi_telemetry/realtime_taxi_telemetry_stack.py
@@ -1,7 +1,7 @@
+import aws_cdk as cdk
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_kinesis as kinesis,
 )
 from constructs import Construct
 
@@ -10,10 +10,12 @@ class RealtimeTaxiTelemetryStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "RealtimeTaxiTelemetryQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Kinesis Data Stream for ingesting trip events
+        self.taxi_event_stream = kinesis.Stream(
+            self,
+            "TaxiEventStream",
+            stream_name="TaxiTripEventStream",
+            shard_count=2,  # supports ~2000 records/sec baseline
+            stream_mode=kinesis.StreamMode.PROVISIONED,  # or .ON_DEMAND for auto-scaling
+            retention_period=cdk.Duration.hours(24),  # Retain events for 24 hours for replay/debug
+        )
